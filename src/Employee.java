797a25f8583715b767a9251a9434344b2e8fd21b
inix84 <inix84@mail.ru>
2022-11-04 04:44:50 +0500

    2 commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,58 @@
+//package course;
+
 public class Employee {
+    private int salary;
+    private final String fullName;
+
+    private final int id;
+
+    private static int counter = 0;
+
+
+    public Employee(String fullName, int salary) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.id = counter++;
+
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    //@Override
+    //public String toString() {
+        //return "Employee {" +
+             //   "salary = " + salary +
+             //   ", fullName ' " + fullName + '\'' +
+             //   '}';
+
+    //}
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee {" +
+                "salary = " + salary +
+                ", fullName ' " + fullName + '\'' +
+                ", id" + id +
+                '}';
+    }
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+//package course;
+
 public class Main {
+    private static final Employee[] employees = new Employee[5];
     public static void main(String[] args) {
+        employees[0] = new Employee("Иванов Иван1 Иванович", 100);
+        employees[1] = new Employee("Иванов Иван2 Иванович",200);
+        employees[2] = new Employee("Иванов Иван3 Иванович",300);
+        employees[3] = new Employee("Иванов Иван4 Иванович",400);
+        employees[4] = new Employee("Иванов Иван5 Иванович",500);
+        System.out.println("Сумма всех зарплат:" + calculateSumSalary() );
+        Employee employeeWithMaxSalary = getEmployeeWithMaxSalary();
+        if (employeeWithMaxSalary == null) {
+            System.out.println("Нет сотрудника с максимальной зарплатой: ");
+        } else {
+            System.out.println("Сотрудник с максимальной зарплатой: " + getEmployeeWithMaxSalary());
+        }
+    }
 
-
+    public static  int calculateSumSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+    public static Employee getEmployeeWithMaxSalary () {
+        int max = Integer.MIN_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : employees) {
+            if (employee !=null && employee.getSalary() > max) {
+                max = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
     }
 }
